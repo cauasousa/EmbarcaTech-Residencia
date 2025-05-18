b6beb77232af4623ca744203d88c48bab53e4c12
cauasousa <llkauall055@gmail.com>
2025-05-18 13:23:52 -0300

    -

--- unidade_1_capitulo_5/unidade_1_capitulo_5.c
+++ unidade_1_capitulo_5/unidade_1_capitulo_5.c
@@ -1,32 +1,35 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+// Inclusão das bibliotecas auxiliares
 #include "inc/ssd1306.h"
 #include "helpers/setup/setup.h"
 #include "helpers/display_oled/display_oled.h"
 #include "helpers/temperature_dma_adc/temperature_dma_adc.h"
 
+// Variável para armazenar a temperatura
 float temp = 0.0f;
 
+// Função chamada a cada segundo para atualizar o display
 bool print_display(struct repeating_timer *t)
 {
-    temp = read_temperature();
+    temp = read_temperature(); // Lê a temperatura do sensor
 
     char temp_text[20];
-    snprintf(temp_text, sizeof(temp_text), "Temp: %.1f C", temp);
-    char *text[] = {temp_text};
+    snprintf(temp_text, sizeof(temp_text), "Temp: %.1f C", temp); // Formata o texto da temperatura
+    char *text[] = {temp_text};                                   // Cria um array de strings para enviar ao display
 
-    write_display(text);
+    write_display(text); // Escreve no display OLED
 
-    return true;
+    return true; // Retorna verdadeiro para continuar repetindo
 }
 
 int main()
 {
-    setup();
+    setup(); // Inicializa os periféricos (display, ADC, DMA.)
 
     struct repeating_timer timer;
-    add_repeating_timer_ms(1000, print_display, NULL, &timer);
+    add_repeating_timer_ms(1000, print_display, NULL, &timer); // Chama print_display a cada 1000 ms
 
     while (true)
     {
